967a2b3d979b6d7b53b74b38c4b55889e68d99ac
Karol Belina <karolbelina@gmail.com>
2018-06-17 01:28:49 +0200

    AI move algorithm finished

--- lab5.asm
+++ lab5.asm
@@ -7,21 +7,48 @@
 # _ 0 X 
 
 .data
-#board: .space 9 # 0 is empty, 1 is player, 2 is ai
+# 0 is empty, 1 is player, 2 is ai
+# 0 1 2
+# 3 4 5
+# 6 7 8
+#board: .space 9
 board: .byte 1,1,0,2,1,2,0,0,1
-row_preferences: .space 8 # top, middle, bottom, left, center, right, diagonal (\), backdiagonal (/), 0 for neutral, -1 and -2 for player, 1 and 2 for ai
 
-top: .byte 0, 1, 2
-middle: .byte 3, 4, 5
-bottom: .byte 6, 7, 8
-left: .byte 0, 3, 6
-center: .byte 1, 4, 7
-right: .byte 2, 5, 8
-diagonal: .byte 0, 4, 8
-backdiagonal: .byte 2, 4, 6
+# 0 - top, 1 - middle, 2 - bottom, 3 - left, 4 - center, 5 - right, 6 - diagonal (\), 7 - backdiagonal (/)
+# 0 for neutral, 1 and 2 for player, 3 and 4 for ai, 5 for wasted
+row_preferences: .space 8 
 
-indexes: .byte 0, 1, 2, 3, 4, 5, 6, 7, 8, 0, 3, 6, 1, 4, 7, 2, 5, 8, 0, 4, 8, 2, 4, 6
+#top: .byte 0, 1, 2
+#middle: .byte 3, 4, 5
+#bottom: .byte 6, 7, 8
+#left: .byte 0, 3, 6
+#center: .byte 1, 4, 7
+#right: .byte 2, 5, 8
+#diagonal: .byte 0, 4, 8
+#backdiagonal: .byte 2, 4, 6
 
+# 0 --x-> 1
+# 0 --o-> 3
+# 1 --x-> 2
+# 1 --o-> 5
+# 2 --x-> x's win
+# 2 --o-> 5
+# 3 --x-> 5
+# 3 --o-> 4
+# 4 --x-> 5
+# 4 --o-> o's win
+# 5 --x-> 5
+# 5 --o-> 5
+#                                        0x 0o 1x 1o 2x 2o 3x 3o 4x 4o 5x 5o
+row_preference_state_lookup_table: .byte 1, 3, 2, 5, 6, 5, 5, 4, 5, 7, 5, 5
+
+# 8 is null
+cell_to_rows: .byte 0, 3, 6, 8,    0, 4, 8, 8,    0, 5, 7, 8,
+                    1, 3, 8, 8,    1, 4, 6, 7,    1, 5, 8, 8,
+                    2, 3, 7, 8,    2, 4, 8, 8,    2, 5, 6, 8
+
+row_to_cells: .byte 0, 1, 2,    3, 4, 5,    6, 7, 8,    0, 3, 6,    1, 4, 7,    2, 5, 8,    0, 4, 8,    2, 4, 6
+                    
 start_msg: .asciiz "Enter the number of matches: "
 start_match_msg: .asciiz "Match #"
 semicolon: .asciiz ": "
@@ -39,27 +66,48 @@ main:
 	li $v0, 10
 	syscall
 
+# a0 - cell index
+# a1 - 0 - x, 1 - o
+update_row_preference_state:
+	
 
 ai_move:
 	li $v0, -1 # preferenced index, default value is -1
-	check_for_lethal_to_player: # check for 2s or -2s
+	check_for_lethal: # check for 2s or -2s
 		li $t0, 0 # initialize the iterator (0-7)
-		check_for_lethal_to_player_row:
+		check_for_lethal_row:
 			lb $t1, row_preferences($t0) # get the preference of the row
-			beq $t1, 2, check_for_lethal_to_player_row_found_2
+			beq $t1, 2, check_for_lethal_found
 			nop
-			beq $t1, -2, check_for_lethal_to_player_row_found_2
+			beq $t1, -2, check_for_lethal_found
 			nop
 			# didn't found 2 or -2
 			addiu $t0, $t0, 1
-			blt $t0, 8, check_for_lethal_to_player_row
-			nop
-			# $t0 is equal to 8
-			#j check_for_lethal_to_ai
+			blt $t0, 8, check_for_lethal_row
 			nop
 			# found the 2 or -2, exit
-			check_for_lethal_to_player_row_found_2:
-			move $v0, $t0 # set the preferenced row index
+			check_for_lethal_found:
+			# get the only empty cell of row in $t0
+			mul $t0, $t0, 3 # multiply times 3 for row_to_cells lookup
+			lb $v0, row_to_cells+0($t0)
+			beqz $v0, ai_move_return
+			nop
+			lb $v0, row_to_cells+1($t0)
+			beqz $v0, ai_move_return
+			nop
+			lb $v0, row_to_cells+2($t0)
+			beqz $v0, ai_move_return
+			nop
+	random_cell:
+		# generate a random integer between 0 and 7
+		li $a0, 0
+		li $a1, 8
+		syscall
+		# check if the cell is empty
+		lb $t0, board($a0)
+		bnez $t0, random_cell
+		nop
+		move $v0, $t0
 	ai_move_return:
 		jr $ra
 		nop
